74caee235ab8fef4e69ddabb90a1b9ce4254b04c
Adriano Dantas <adrianodts@msn.com>
2021-02-12 00:18:37 -0300

    Refactor-1b Separando arquivos statement e createStatementData

--- statement.js
+++ statement.js
@@ -1,70 +1,9 @@
-function statement (invoice, plays) {
-    const statementData = {};
-    statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances.map(enrichPerformance);    
-    statementData.totalAmount = totalAmount(statementData);
-    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-    return renderPlaintext(statementData, plays);
-    
-    function enrichPerformance(aPerformace) {
-        const result = Object.assign({}, aPerformace);
-        result.play = playFor(result);
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
-        return result;
-    }
+import createStatementData from './createStatementData.js'
 
-    function playFor(aPerformace) {
-        return plays.find((play) => { 
-            return play.playID === aPerformace.playID
-        });
-    }
-
-    function amountFor(aPerformace) {
-        let result = 0;
-        switch (aPerformace.play.type) {
-            case "tragedy":
-                result = 40000;
-                if(aPerformace.audience > 30) {
-                    result += 1000 * (aPerformace.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if(aPerformace.audience > 20) {
-                    result += 10000 + 500 * (aPerformace.audience - 20);
-                }
-                result += 300 * aPerformace.audience;
-                break;
-            default:
-                throw new Error(`unknow type: ${playFor(aPerformace).type}`);
-        }
-        return result;
-    }
-
-    function volumeCreditsFor(aPerformace) {
-        let result = 0;
-        result += Math.max(aPerformace.audience -30, 0);
-        if ("comedy" === aPerformace.play.type) result += Math.floor(aPerformace.audience / 5)
-        return result;
-    }
-
-    function totalAmount(data) {
-        let result = 0;
-        for(let perf of data.performances) {
-            result += perf.amount;
-        }
-        return result;
-    }
-
-    function totalVolumeCredits(data) {
-        let result = 0;
-        for(let perf of data.performances) {
-            result += perf.volumeCredits;
-        }
-        return result;
-    }
+export default function statement (invoice, plays) {
+    return renderPlaintext(createStatementData(invoice, plays));
 }
+
 function renderPlaintext (data, plays) {
     let result = `Statement for ${data.customer}\n`;
     for(let perf of data.performances) {
@@ -82,4 +21,3 @@ function renderPlaintext (data, plays) {
         }).format(aNumber/100);
     }
 }
-module.exports = statement
